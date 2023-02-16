fd72021e1de4d03371cb50883605b7998434ae92
Mike Melchior <mike.melchior@outlook.com>
2023-02-16 16:21:21 -0500

    commit

---
+++ dom.bundle.js
@@ -0,0 +1,19 @@
+(self["webpackChunktodo_list"] = self["webpackChunktodo_list"] || []).push([["dom"],{
+
+/***/ "./src/dom-stuff.js":
+/*!**************************!*\
+  !*** ./src/dom-stuff.js ***!
+  \**************************/
+/***/ (() => {
+
+const contentDiv = document.createElement('div');
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/dom-stuff.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZG9tLmJ1bmRsZS5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7OztBQUFBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdG9kby1saXN0Ly4vc3JjL2RvbS1zdHVmZi5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zdCBjb250ZW50RGl2ID0gZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgnZGl2Jyk7Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
