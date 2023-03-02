bf35f2dd70ef6e522eebbd7c7a900b421831fa8a
Mike Melchior <mike.melchior@outlook.com>
2023-03-02 13:03:50 -0500

    change title

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Development'
+            title: 'Todo List App'
         }),
     ],
     module: {
